6cdbd2d4c1f6f83de18c9d20d07494cee10cbe88
youaresogay22 <youaresogayee@gmail.com>
2024-03-04 14:38:23 +0900

    rest api 실습

--- src/main/java/com/nhnacademy/edu/springboot/account/service/DefaultAccountService.java
+++ src/main/java/com/nhnacademy/edu/springboot/account/service/DefaultAccountService.java
@@ -4,12 +4,12 @@ import com.nhnacademy.edu.springboot.account.domain.Account;
 import com.nhnacademy.edu.springboot.account.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class DefaultAccountService implements AccountService {
-
     private final AccountRepository accountRepository;
 
     @Autowired
@@ -18,21 +18,28 @@ public class DefaultAccountService implements AccountService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Account> getAccounts() {
         return accountRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Account getAccount(Long id) {
-        return accountRepository.findById(id).orElse(null);
+        return accountRepository.findById(id).orElseThrow();
     }
 
     @Override
+    @Transactional
     public Account createAccount(Account account) {
+        boolean present = accountRepository.findById(account.getId()).isPresent();
+        if (present) throw new IllegalStateException("already exist " + account.getId());
+
         return accountRepository.save(account);
     }
 
     @Override
+    @Transactional
     public void deleteAccount(Long id) {
         accountRepository.deleteById(id);
     }
